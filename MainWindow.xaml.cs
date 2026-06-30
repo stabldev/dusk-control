d61e915c09546a962d0186829c2c4b5d98c08934
stabldev <114811070+stabldev@users.noreply.github.com>
2026-06-30 17:16:49 +0530

    feat: show main-window at right bottom corner

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Windowing;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,6 +21,25 @@ public sealed partial class MainWindow : Window
     SetTitleBar(AppTitleBar);
 
     AppWindow.SetIcon("Assets/AppIcon.ico");
+    AppWindow.Resize(new Windows.Graphics.SizeInt32(400, 300));
+
+    if (AppWindow.Presenter is OverlappedPresenter overlappedPresenter)
+    {
+      overlappedPresenter.IsResizable = false;
+      overlappedPresenter.IsMaximizable = false;
+      overlappedPresenter.IsMinimizable = false;
+    }
+
+    // Position window at bottom right corner above the taskbar with some padding
+    var displayArea = DisplayArea.Primary;
+    if (displayArea != null)
+    {
+      var workArea = displayArea.WorkArea;
+      int padding = 6;
+      int x = workArea.X + workArea.Width - AppWindow.Size.Width - padding;
+      int y = workArea.Y + workArea.Height - AppWindow.Size.Height - padding;
+      AppWindow.Move(new Windows.Graphics.PointInt32(x, y));
+    }
 
     // Navigate the root frame to the main page on startup.
     RootFrame.Navigate(typeof(MainPage));
