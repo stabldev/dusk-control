fb8d23b90c9c82738842bc0fd1795f7607a7754a
stabldev <114811070+stabldev@users.noreply.github.com>
2026-07-04 11:40:31 +0530

    fix: configure site and base for gh-pages

--- Docs/astro.config.mjs
+++ Docs/astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+const isGithub = process.env.GITHUB_ACTIONS === "true";
+
 // https://astro.build/config
 export default defineConfig({
+  site: isGithub ? "https://stabldev.github.io" : undefined,
+  base: isGithub ? "/dusk-control" : undefined,
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
