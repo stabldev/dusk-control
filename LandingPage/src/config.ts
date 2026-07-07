b7e7ca7fae19bc30d3fca3bdb14e375857c6d141
stabldev <114811070+stabldev@users.noreply.github.com>
2026-07-07 10:55:06 +0530

    chore: bump version to v1.0.1

--- LandingPage/src/config.ts
+++ LandingPage/src/config.ts
@@ -1,4 +1,4 @@
-export const VERSION = "1.0.0";
+export const VERSION = "1.0.1";
 export const GITHUB_REPO_URL = "https://github.com/stabldev/dusk-control";
 export const RELEASES_URL = `${GITHUB_REPO_URL}/releases`;
 export const DOWNLOAD_INSTALLER_URL = `${RELEASES_URL}/download/v${VERSION}/DuskControl.v${VERSION}.exe`;
