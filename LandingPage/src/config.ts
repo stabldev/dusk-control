d42818c49ffe5e8aaa4cac1c20f32ed77c35afb0
stabldev <114811070+stabldev@users.noreply.github.com>
2026-07-10 15:09:05 +0530

    chore: add microsoft store links to landing page

--- LandingPage/src/config.ts
+++ LandingPage/src/config.ts
@@ -2,3 +2,5 @@ export const VERSION = "1.0.5";
 export const GITHUB_REPO_URL = "https://github.com/stabldev/dusk-control";
 export const RELEASES_URL = `${GITHUB_REPO_URL}/releases`;
 export const DOWNLOAD_INSTALLER_URL = `${RELEASES_URL}/download/v${VERSION}/DuskControl.v${VERSION}.exe`;
+export const MICROSOFT_STORE_URL =
+  "https://apps.microsoft.com/detail/9PC2NH8VN5NT?referrer=appbadge&mode=full";
