af12507c474624b839cbb2109d2dc0d144613a2f
stabldev <114811070+stabldev@users.noreply.github.com>
2026-07-18 10:20:02 +0530

    feat: start minimized in tray when started with windows

--- App.xaml.cs
+++ App.xaml.cs
@@ -32,6 +32,11 @@ public partial class App : Application
   protected override void OnLaunched(LaunchActivatedEventArgs args)
   {
     _window = new MainWindow();
-    _window.Activate();
+
+    var cmdArgs = Environment.GetCommandLineArgs();
+    if (!cmdArgs.Contains("--hidden"))
+    {
+      _window.Activate();
+    }
   }
 }
